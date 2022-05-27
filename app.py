150e0aac97d329bd6b24ba2956b929df6b4381c3
RachanaMangalaram <102574999+RachanaMangalaram@users.noreply.github.com>
2022-05-27 17:55:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,30 +1,52 @@
-from flask import Flask,render_template,Response
-import cv2
+from flask import Flask,render_template,request
+import mysql.connector
+app=Flask(__name__, template_folder = 'template')
 
-app = Flask(__name__, template_folder='template/HTML')
-camera = cv2.VideoCapture(0)
+mydb=mysql.connector.connect(host="localhost",user="root",passwd="123456")
 
 
-def generate_frames():
-    while True:
-        ## read the camera frame
-        success,frame = camera.read()
-        if not success:
-            break
-        else:
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
+if mydb.is_connected():
+    print("Successfully Connected...")
+else:
+    print("Some Connectivity Issue Occured....")
 
-        yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+#creating the database
+# mycursor=mydb.cursor()
+# query="create database orchid;"
+# mycursor.execute(query)
 
+# mycursor=mydb.cursor()
+# mycursor.execute("use xyz")
+# query = "create table User1(roll int , name varchar(20))"
+# mycursor.execute(query)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("sign_up.html")
 
-@app.route('/video')
-def video():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/login', methods=['POST','GET'])
+def p():
+    if request.method=='POST':
+        user=request.form['Email']
+        age=request.form['Age']
+        password=request.form['Password']
+        cpass=request.form['cpass']
+        mycursor=mydb.cursor()
+        mycursor.execute("use  abc")
+        sql = "insert into user4(email,Age,password) values(%s, %s,%s)"
+        val = (user,age,password)
+        mycursor.execute(sql, val)
+        mydb.commit()
+        mydb.rollback()
+        print("Data Inserted Successfully......")
+        mydb.close()
+            
+        if password==cpass:
+            print(user)
+            print(age)
+            print(password)
+            print(cpass)
+            return render_template('get.html', uname=user,a=age,pas=password,cpassword=cpass)
 
-if __name__=="__main__":
+if __name__=="main_":
     app.run(debug=True)
