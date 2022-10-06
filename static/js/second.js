b94299455b20972a294e85adda6f6652f7be6f6a
Rathodpruthvi45 <102455345+Rathodpruthvi45@users.noreply.github.com>
2022-10-06 19:31:32 +0530

    Add files via upload

---
+++ static/js/second.js
@@ -0,0 +1,323 @@
+
+
+
+var category=localStorage.getItem("l");
+
+if(category=="Hospital"){
+    console.log(category);
+   
+    document.getElementById("img1").src ="ambulance.svg";
+    document.getElementById("img2").src ="breathe.png";
+    document.getElementById("img3").src ="cold.png";
+    document.getElementById("img4").src ="difficult to.png";
+    document.getElementById("img5").src ="doctor.png";
+    document.getElementById("img6").src ="fever.png";
+    document.getElementById("img7").src ="good.png";
+    document.getElementById("img8").src ="happy.svg";
+    document.getElementById("img9").src ="high.svg";
+    document.getElementById("img10").src ="low.svg";
+    document.getElementById("img11").src ="injection.png";
+    document.getElementById("img12").src ="logbook.png";
+    document.getElementById("img13").src ="i am.png";
+    document.getElementById("img14").src ="medical_report.png";
+    document.getElementById("img15").src ="medicines.png";
+    document.getElementById("img16").src ="need.png";
+    document.getElementById("img17").src ="not well.png";
+    document.getElementById("img18").src ="nurse.png";
+    document.getElementById("img19").src ="patient.png";
+    document.getElementById("img20").src ="pillow.png";
+    document.getElementById("img21").src ="pulse.svg";
+    document.getElementById("img22").src ="rest.png";
+    document.getElementById("img23").src ="saline.png";
+    document.getElementById("img24").src ="sore throat.png";
+
+
+
+  document.getElementById("b1").innerHTML="Ambulance ";
+  document.getElementById("b2").innerHTML="Breathe ";
+  document.getElementById("b3").innerHTML="Cold ";
+  document.getElementById("b4").innerHTML="Difficult to ";
+  document.getElementById("b5").innerHTML="Doctor ";
+  document.getElementById("b6").innerHTML="Fever ";
+  document.getElementById("b7").innerHTML="Good ";
+  document.getElementById("b8").innerHTML="Happy ";
+  document.getElementById("b9").innerHTML="High ";
+  document.getElementById("b10").innerHTML="Low";
+  document.getElementById("b11").innerHTML="Injection ";
+  document.getElementById("b12").innerHTML="Logbook ";
+  document.getElementById("b13").innerHTML="I am";
+  document.getElementById("b14").innerHTML="Medical Report ";
+  document.getElementById("b15").innerHTML="Medicines ";
+  document.getElementById("b16").innerHTML="Need ";
+  document.getElementById("b17").innerHTML="Not well ";
+  document.getElementById("b18").innerHTML="Nurse "; 
+  document.getElementById("b19").innerHTML="Patient ";
+  document.getElementById("b20").innerHTML="Pillow ";
+  document.getElementById("b21").innerHTML="Pulse ";
+  document.getElementById("b22").innerHTML="Rest ";
+  document.getElementById("b23").innerHTML="Saline ";
+  document.getElementById("b24").innerHTML="Sore throat ";
+
+
+  
+
+}
+else if(category=="Bank"){
+    document.getElementById("img1").src ="withdraw.png";
+    document.getElementById("img2").src ="money.png";
+    document.getElementById("img3").src ="call.png";
+    document.getElementById("img4").src ="loan.png";
+    document.getElementById("img5").src ="deposit.png";
+    document.getElementById("img6").src ="want.svg";
+    document.getElementById("img7").src ="i.svg";
+    document.getElementById("img8").src ="have.png";
+    document.getElementById("img9").src ="Bank.png";
+    document.getElementById("img10").src ="account.png";
+    document.getElementById("img11").src ="cashier.png";
+    document.getElementById("img12").src ="blockacc.png";
+    document.getElementById("img13").src ="cheque.png";
+    document.getElementById("img14").src ="interest.png";
+    document.getElementById("img15").src ="no.svg";
+    document.getElementById("img16").src ="thanks.png";
+    document.getElementById("img17").src ="please.png";
+    document.getElementById("img18").src ="yess.png";
+    document.getElementById("img19").src ="giveme.png";
+    document.getElementById("img20").src ="got it.png";
+    document.getElementById("img21").src ="whereto.svg";
+    document.getElementById("img22").src ="sign.png";
+    document.getElementById("img23").src ="went.svg";
+    document.getElementById("img24").src ="can i.png";
+
+    document.getElementById("b1").innerHTML="Withdraw ";
+    document.getElementById("b2").innerHTML="Money";
+    document.getElementById("b3").innerHTML="Call";
+    document.getElementById("b4").innerHTML="Loan";
+    document.getElementById("b5").innerHTML="Deposit";
+    document.getElementById("b6").innerHTML="Want";
+    document.getElementById("b7").innerHTML="I";
+    document.getElementById("b8").innerHTML="Have";
+    document.getElementById("b9").innerHTML="Bank";
+    document.getElementById("b10").innerHTML="Account";
+    document.getElementById("b11").innerHTML="Cashier";
+    document.getElementById("b12").innerHTML="Block";
+    document.getElementById("b13").innerHTML="Cheque";
+    document.getElementById("b14").innerHTML="Interest";
+    document.getElementById("b15").innerHTML="No";
+    document.getElementById("b16").innerHTML="Thank You";
+    document.getElementById("b17").innerHTML="Please";
+    document.getElementById("b18").innerHTML="Yes";
+    document.getElementById("b19").innerHTML="Give me";
+    document.getElementById("b20").innerHTML="Got it";
+    document.getElementById("b21").innerHTML="Where to";
+    document.getElementById("b22").innerHTML="Sign";
+    document.getElementById("b23").innerHTML="Went to";
+    document.getElementById("b24").innerHTML="Can I";
+   
+}
+else if(category=="School")
+{
+    document.getElementById("img1").src ="book.png";
+    document.getElementById("img2").src ="pen.svg";
+    document.getElementById("img3").src ="pencil.svg";
+    document.getElementById("img4").src ="bell.png";
+    document.getElementById("img5").src ="bag.svg";
+    document.getElementById("img6").src ="classroom.png";
+    document.getElementById("img7").src ="fees.png";
+    document.getElementById("img8").src ="yours.png";
+    document.getElementById("img9").src ="test.svg";
+    document.getElementById("img10").src ="library.svg";
+    document.getElementById("img11").src ="principal.png";
+    document.getElementById("img12").src ="tiffin.svg";
+    document.getElementById("img13").src ="bottle.png";
+    document.getElementById("img14").src ="sit.png";
+    document.getElementById("img15").src ="give.png";
+    document.getElementById("img16").src ="take.png";
+    document.getElementById("img17").src ="eat.png";
+    document.getElementById("img18").src ="read.png";
+    document.getElementById("img19").src ="write.png";
+    document.getElementById("img20").src ="my.png";
+    document.getElementById("img21").src ="school bus.svg";
+    document.getElementById("img22").src ="math.svg";
+    document.getElementById("img23").src ="computer.png";
+    document.getElementById("img24").src ="speech.png";
+
+    document.getElementById("b1").innerHTML="Book ";
+    document.getElementById("b2").innerHTML="Pen";
+    document.getElementById("b3").innerHTML="Pencil"; 
+    document.getElementById("b4").innerHTML="Bell";
+    document.getElementById("b5").innerHTML="Bag ";
+    document.getElementById("b6").innerHTML="Classroom";
+    document.getElementById("b7").innerHTML="Fees";
+    document.getElementById("b8").innerHTML="Yours";
+    document.getElementById("b9").innerHTML="Test";
+    document.getElementById("b10").innerHTML="Library";
+    document.getElementById("b11").innerHTML="Principal";
+    document.getElementById("b12").innerHTML="Tiffin";
+    document.getElementById("b13").innerHTML="Bottle";
+    document.getElementById("b14").innerHTML="Sit";
+    document.getElementById("b15").innerHTML="Give";
+    document.getElementById("b16").innerHTML="Take";
+    document.getElementById("b17").innerHTML="Eat";
+    document.getElementById("b18").innerHTML="Read ";
+    document.getElementById("b19").innerHTML="Write ";
+    document.getElementById("b20").innerHTML="My";
+    document.getElementById("b21").innerHTML="School Bus";
+    document.getElementById("b22").innerHTML="Math Class";
+    document.getElementById("b23").innerHTML="IT Class";
+    document.getElementById("b24").innerHTML="Speech";
+}
+else if(category=="Restaurant"){
+    document.getElementById("img1").src ="bill.svg";
+    document.getElementById("img2").src ="birthday.png";
+    document.getElementById("img3").src ="cashier.png";
+    document.getElementById("img4").src ="cheese.png";
+    document.getElementById("img5").src ="chef.png";
+    document.getElementById("img6").src ="chinese_food.svg";
+    document.getElementById("img7").src ="delicious food.svg";
+    document.getElementById("img8").src ="your.png";
+    document.getElementById("img9").src ="desserts.png";
+    document.getElementById("img10").src ="french_fries.svg";
+    document.getElementById("img11").src ="juice.png";
+    document.getElementById("img12").src ="menu.svg";
+    document.getElementById("img13").src ="money.png";
+    document.getElementById("img14").src ="no.png";
+    document.getElementById("img15").src ="pay.png";
+    document.getElementById("img16").src ="pizza.png";
+    document.getElementById("img17").src ="sorry.svg";
+    document.getElementById("img18").src ="serving.png";
+    document.getElementById("img19").src ="sweets.png";
+    document.getElementById("img20").src ="thankyou.svg";
+    document.getElementById("img21").src ="tissue.svg";
+    document.getElementById("img22").src ="waiter.svg";
+    document.getElementById("img23").src ="sauce.svg";
+    document.getElementById("img24").src ="spoons.svg";
+
+    document.getElementById("b1").innerHTML="Bill";
+    document.getElementById("b2").innerHTML="Birthday";
+    document.getElementById("b3").innerHTML="Cashier"; 
+    document.getElementById("b4").innerHTML="Cheese";
+    document.getElementById("b5").innerHTML="Chef";
+    document.getElementById("b6").innerHTML="Chinese food";
+    document.getElementById("b7").innerHTML="Delicious food";
+    document.getElementById("b8").innerHTML="Your";
+    document.getElementById("b9").innerHTML="Desserts";
+    document.getElementById("b10").innerHTML="French fries";
+    document.getElementById("b11").innerHTML="Juice";
+    document.getElementById("b12").innerHTML="Menu";
+    document.getElementById("b13").innerHTML="Money";
+    document.getElementById("b14").innerHTML="No";
+    document.getElementById("b15").innerHTML="Pay";
+    document.getElementById("b16").innerHTML="Pizza";
+    document.getElementById("b17").innerHTML="Sorry!";
+    document.getElementById("b18").innerHTML="Serving";
+    document.getElementById("b19").innerHTML="Sweets";
+    document.getElementById("b20").innerHTML="Thank You";
+    document.getElementById("b21").innerHTML="Tissue";
+    document.getElementById("b22").innerHTML="Waiter";
+    document.getElementById("b23").innerHTML="Sauce";
+    document.getElementById("b24").innerHTML="Spoons";
+}
+else if(category=="General"){
+    document.getElementById("img1").src ="I want.png";
+    document.getElementById("img2").src ="toothpaste.svg";
+    document.getElementById("img3").src ="shampoo.svg";
+    document.getElementById("img4").src ="cheese.png";
+    document.getElementById("img5").src ="milk.svg";
+    document.getElementById("img6").src ="peanuts.png";
+    document.getElementById("img7").src ="almonds.png";
+    document.getElementById("img8").src ="biscuits.svg";
+    document.getElementById("img9").src ="sugar.png";
+    document.getElementById("img10").src ="salt.svg";
+    document.getElementById("img11").src ="detergent.png";
+    document.getElementById("img12").src ="clove.png";
+    document.getElementById("img13").src ="coconut.svg";
+    document.getElementById("img14").src ="cost.svg";
+    document.getElementById("img15").src ="tooth brush.png";
+    document.getElementById("img16").src ="what's the.png";
+    document.getElementById("img17").src ="bread.svg";
+    document.getElementById("img18").src ="oil.png";
+    document.getElementById("img19").src ="tissue paper.svg";
+    document.getElementById("img20").src ="chips.png";
+    document.getElementById("img21").src ="chocolate.svg";
+    document.getElementById("img22").src ="two.svg";
+    document.getElementById("img23").src ="pen.svg";
+    document.getElementById("img24").src ="book.png";
+
+    document.getElementById("b1").innerHTML="I want";
+    document.getElementById("b2").innerHTML="Toothpaste";
+    document.getElementById("b3").innerHTML="Shampoo";
+    document.getElementById("b4").innerHTML="Cheese";
+    document.getElementById("b5").innerHTML="Milk";
+    document.getElementById("b6").innerHTML="Peanuts";
+    document.getElementById("b7").innerHTML="Almonds";
+    document.getElementById("b8").innerHTML="Biscuits";
+    document.getElementById("b9").innerHTML="Sugar";
+    document.getElementById("b10").innerHTML="Salt";
+    document.getElementById("b11").innerHTML="Detergent powder";
+    document.getElementById("b12").innerHTML="Clove";
+    document.getElementById("b13").innerHTML="Coconut";
+    document.getElementById("b14").innerHTML="Cost";
+    document.getElementById("b15").innerHTML="Tooth brush";
+    document.getElementById("b16").innerHTML="What's the";
+    document.getElementById("b17").innerHTML="Bread";
+    document.getElementById("b18").innerHTML="Oil";
+    document.getElementById("b19").innerHTML="Tissue paper";
+    document.getElementById("b20").innerHTML="Chips";
+    document.getElementById("b21").innerHTML="Chocolate";
+    document.getElementById("b22").innerHTML="Two";
+    document.getElementById("b23").innerHTML="Pen";
+    document.getElementById("b24").innerHTML="Book";
+    
+  
+}
+else if(category=="Market"){
+    document.getElementById("img1").src ="tomato.png";
+    document.getElementById("img2").src ="lemon.png";
+    document.getElementById("img3").src ="potato.png";
+    document.getElementById("img4").src ="fruits.png";
+    document.getElementById("img5").src ="banana.svg";
+    document.getElementById("img6").src ="cabbages.png";
+    document.getElementById("img7").src ="cucumber.png";
+    document.getElementById("img8").src ="garlic.png";
+    document.getElementById("img9").src ="ginger.png";
+    document.getElementById("img10").src ="carrot.png";
+    document.getElementById("img11").src ="capsicum.png";
+    document.getElementById("img12").src ="coriander.png";
+    document.getElementById("img13").src ="broccoli.png";
+    document.getElementById("img14").src ="onion.png";
+    document.getElementById("img15").src ="what is.svg";
+    document.getElementById("img16").src ="cost.png";
+    document.getElementById("img17").src ="one.png";
+    document.getElementById("img18").src ="two.png";
+    document.getElementById("img19").src ="half.png";
+    document.getElementById("img20").src ="give me.png";
+    document.getElementById("img21").src ="thankyou.png";
+    document.getElementById("img22").src ="yes.png";
+    document.getElementById("img23").src ="fresh vegetables.png";
+    document.getElementById("img24").src ="no.png";
+
+    document.getElementById("b1").innerHTML="Tomato";
+    document.getElementById("b2").innerHTML="Lemon";
+    document.getElementById("b3").innerHTML="Potato";
+    document.getElementById("b4").innerHTML="Fruits";
+    document.getElementById("b5").innerHTML="Banana";
+    document.getElementById("b6").innerHTML="Cabbage";
+    document.getElementById("b7").innerHTML="Cucumber";
+    document.getElementById("b8").innerHTML="Garlic";
+    document.getElementById("b9").innerHTML="Ginger";
+    document.getElementById("b10").innerHTML="Carrot";
+    document.getElementById("b11").innerHTML="Capsicum";
+    document.getElementById("b12").innerHTML="Coriander";
+    document.getElementById("b13").innerHTML="Broccoli";
+    document.getElementById("b14").innerHTML="Onion";
+    document.getElementById("b15").innerHTML="What is ";
+    document.getElementById("b16").innerHTML="Rate of ";
+    document.getElementById("b17").innerHTML="One Kg";
+    document.getElementById("b18").innerHTML="Two Kg";
+    document.getElementById("b19").innerHTML="1/2kg";
+    document.getElementById("b20").innerHTML="Give me";
+    document.getElementById("b21").innerHTML="Thank You";
+    document.getElementById("b22").innerHTML="Yes";
+    document.getElementById("b23").innerHTML="Vegetables";
+    document.getElementById("b24").innerHTML="No";
+}
---
+++ static/js/virtual.js
@@ -0,0 +1,39 @@
+const div1 = document.querySelector('.box1');
+div1.addEventListener('click', (event) => {
+  localStorage.setItem("l", "Hospital");
+ 
+  
+  window.open('keybored.html');
+ 
+});
+
+const div2 = document.querySelector('.box2');
+div2.addEventListener('click', (event) => {
+  localStorage.setItem("l", "Bank");
+  
+  window.open('keybored.html')
+});
+
+const div3 = document.querySelector('.box3');
+div3.addEventListener('click', (event) => {
+  localStorage.setItem("l", "School");
+  window.open('keybored.html')
+});
+
+const div4 = document.querySelector('.box4');
+div4.addEventListener('click', (event) => {
+  localStorage.setItem("l", "Restaurant");
+  window.open('keybored.html')
+});
+
+const div5 = document.querySelector('.box5');
+div5.addEventListener('click', (event) => {
+  localStorage.setItem("l", "General");
+  window.open('keybored.html')
+});
+
+const div6 = document.querySelector('.box6');
+div6.addEventListener('click', (event) => {
+  localStorage.setItem("l", "Market");
+  window.open('keybored.html')
+});
